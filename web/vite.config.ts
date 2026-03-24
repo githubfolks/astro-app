c7f20b200ec2be4cd5382616557d04c97876f116
Vikram Kumar <vikram.bizz@gmail.com>
2026-03-24 12:09:14 +0530

    page speed fixes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -33,10 +33,18 @@ export default defineConfig({
     build: {
         target: 'esnext',
         cssCodeSplit: true,
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true
+            }
+        },
         rollupOptions: {
             output: {
                 manualChunks: {
-                    vender: ['react', 'react-dom', 'react-router-dom', 'lucide-react']
+                    'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+                    'vendor-ui': ['lucide-react', 'aos']
                 }
             }
         }
